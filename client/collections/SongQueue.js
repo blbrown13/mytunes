cdbfcc3fe13660b514ea68c1a9ccc73910f85ac3
Hack Reactor Student <student@hackreactor.com>
2017-04-10 21:10:40 -0700

    Update song and song queue models

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,16 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if ( this.length === 1 ) {
+        this.playFirst();
+      }
+    }, this);
+  },
+
+  playFirst: function() {
+    var song = this.at(0);
+    song.play();
   }
 
 });
